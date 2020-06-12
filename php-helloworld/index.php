5911f2dff4c9c463d8197b791f63e67fcfcc295e
Student User <student@workstation.lab.example.com>
2020-06-12 18:58:28 +0000

    This is for Aamina

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,16 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "I Love YOU AAMINA MY Darling AAAsheekasheeeeekkk! \n "; 
+#print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "I Love YOU AAMINA MY Darling \n ";
+print "You are the world to me my LOVE \n";
+
+print "I Love YOU AAMINA MY Darling \n ";
+print "You are the world to me my LOVE \n";
+
+print "I Love YOU AAMINA MY Darling \n ";
+print "You are the world to me my LOVE \n";
+
+print "I Love YOU AAMINA MY Darling \n ";
+print "You are the world to me my LOVE \n";
+
 ?>
 
