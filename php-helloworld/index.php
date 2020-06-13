741e71c2382eb94ead912b049a63bf264b2ab896
Student User <student@workstation.lab.example.com>
2020-06-13 14:05:16 +0000

    Hope it works

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
-print "Hello this is my first application on openshift" "\n";
-print "And Mr Jaleel you are a waste guy" "\n";
-print " IFNA GROUP ROCKS" "\n";
-print " Thanks for Reading" "\n";
+print "Hello this is my first application on openshit" "\n";
+print "And Mr Jaleel you are a waste guy \n";
+print " IFNA GROUP ROCKS \n";
+print " Thanks for Reading \n";
 ?>
