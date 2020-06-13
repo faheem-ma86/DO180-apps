5d3b2b45dc97191117d679316252b2637c5fa16a
Student User <student@workstation.lab.example.com>
2020-06-13 14:25:06 +0000

    as lab

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hi This is for test" "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+#print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
