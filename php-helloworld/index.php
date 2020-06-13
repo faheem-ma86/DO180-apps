d84ab80685f953caa3a7edb77cb9fbc61edc3a35
Student User <student@workstation.lab.example.com>
2020-06-13 13:11:30 +0000

    Please work

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello this is my first application on openshift" "\r\n";
+print "And Mr Jaleel you are a waste guy" "\r\n";
+print " IFNA GROUP ROCKS" "\r\n";
 ?>
